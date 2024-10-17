bdf4e2d9cc40c33e0ae5b1c2968331f3c14fd523
trancuong19022 <113131294+trancuong19022@users.noreply.github.com>
2024-10-17 14:59:23 +0700

    form hóa đơn

--- DAO/obj/Debug/net8.0/DAO.AssemblyInfo.cs
+++ DAO/obj/Debug/net8.0/DAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+154c7dd21fe058873e9a1905f7e797c1533a052a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d09346e4fa3b0c732531063d7c3147ebdbb675b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- GUI/FormHoaDon.cs
+++ GUI/FormHoaDon.cs
@@ -16,5 +16,57 @@ namespace GUI
         {
             InitializeComponent();
         }
+
+
+        private void Load_Form(object sender, EventArgs e)
+        {
+            lsvThemHoaDon.Columns[0].Width = (int)(lsvThemHoaDon.Width * 0.2);
+            lsvThemHoaDon.Columns[1].Width = (int)(lsvThemHoaDon.Width * 0.2);
+            lsvThemHoaDon.Columns[2].Width = (int)(lsvThemHoaDon.Width * 0.2);
+            lsvThemHoaDon.Columns[3].Width = (int)(lsvThemHoaDon.Width * 0.2);
+            lsvThemHoaDon.Columns[4].Width = (int)(lsvThemHoaDon.Width * 0.2);
+            lsvThemHoaDon.View = View.Details;
+            lsvThemHoaDon.GridLines = true;
+            lsvThemHoaDon.FullRowSelect = true;
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTaoMoiKhachHang_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panelHoaDon_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void lsvThemHoaDon_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormHoaDon_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
